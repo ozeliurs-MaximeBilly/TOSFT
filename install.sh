d92200ee97f8712c41a9dad866421f8925241e6e
BILLY Maxime <ozeliurs@gmail.com>
2021-06-17 18:01:01 +0200

    Create install.sh
    Not Working Yet ... Just a TODO List

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#This is the installation script for The Open Source Fit Tracker
+
+#Not Working yet...
+
+
+# TODO:
+# Install Nginx
+# Clone this repo
+# Move important files to Nginx
+# Install Nginx config
+# Figure out how to manage users and users data
+# Cron job to update the website every ...
+# Javascript ???
+# Have Fun.
