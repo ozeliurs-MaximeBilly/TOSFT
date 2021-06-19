df689a14194d562b979b50f0ff63ba5f2dafccf8
Your Name <you@example.com>
2021-06-19 21:17:17 +0200

    Adding Weight tracking

--- www/app.py
+++ www/app.py
@@ -2,6 +2,7 @@ from flask import Flask, redirect, url_for, render_template, request, session, f
 from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
 import os
+from datetime import date, timedelta
 
 app = Flask(__name__)
 
@@ -11,16 +12,27 @@ app.secret_key = os.environ.get("SECRET_KEY")
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite"
 db = SQLAlchemy(app)
 
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String)
     email = db.Column(db.String)
     password = db.Column(db.String)
 
+
 class Weight(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.Date)
     value = db.Column(db.Float)
     uid = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+
+def last30():
+    list30 = []
+    for i in range(31):
+        list30.append((date.today() - timedelta(days=(30-i))).isoformat())
+    return list30
+
 
 # Functions for website rendering
 @app.route("/")
@@ -42,8 +54,8 @@ def login():
         if existsing_user:
             if existsing_user.password == request.form["password"]:
                 flash("You have been logged in !", "success")
-                session["username"] = request.form["username"]
-                session["password"] = request.form["password"]
+                session["id"] = existsing_user.id
+                session["username"] = User.query.filter_by(id=session["id"]).first().username
                 return redirect(url_for("user"))
             else:
                 flash("Wrong Password.", "error")
@@ -68,8 +80,8 @@ def register():
             db.session.add(nusr)
             db.session.commit()
 
-            session["username"] = request.form["username"]
-            session["password"] = request.form["password"]
+            session["id"] = nusr.id
+            session["username"] = User.query.filter_by(id=session["id"]).first().username
 
             flash("You have been registered !", "success")
             return redirect(url_for("user"))
@@ -80,6 +92,19 @@ def register():
 @app.route("/user/", methods=["GET", "POST"])
 def user():
     return render_template("user.html")
+
+
+@app.route("/weight/", methods=["GET", "POST"])
+def weight():
+    if request.method == "POST":
+
+        return redirect(url_for("weight"))
+    else:
+        uweight = Weight.query.filter_by(uid=session["id"])
+        if uweight:
+            for w in uweight:
+                print(w.value)
+        return render_template("weight.html", labels="'" + "', '".join(last30()) + "'", data="1, 2, 'N/A' , 2.5, 6, 3")
 
 
 @app.route("/info/")
@@ -96,6 +121,5 @@ def logout():
 
 
 if __name__ == "__main__":
-    if not (db.engine.has_table('user')):
-        db.create_all()
+    db.create_all()
     app.run(debug=True)
--- www/testArea.py
+++ www/testArea.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
+from datetime import date, timedelta
 import os
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
-
+print(date.today().isoformat().replace("-","/"))
 print(SECRET_KEY)
